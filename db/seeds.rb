526b87dff2d4631ede97c80673bcf24cb85a65fc
Wojciech Rymaszewski <wrymaszewski@gmail.com>
2016-07-29 15:31:30 +0200

    test commit2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,87 +1,87 @@
 User.destroy_all
-RegularChemical.destroy_all
-Primer.destroy_all
-Sequence.destroy_all
-BacterialStock.destroy_all
-MolBiolChemical.destroy_all
+# RegularChemical.destroy_all
+# Primer.destroy_all
+# Sequence.destroy_all
+# BacterialStock.destroy_all
+# MolBiolChemical.destroy_all
 
 users = User.create! [
   { username: "wrymaszewski", password: "123", first_name: "Wojciech", last_name: "Rymaszewski" },
-  { username: "admin", password: "WIV7289" , first_name: "Admin", last_name: "Admin"}
+  { username: "admin", password: "1234" , first_name: "Admin", last_name: "Admin"}
 ]
 
-require 'csv'
-csv = File.read(Rails.root.join('lib', 'seeds', 'reg_chem.csv')).scrub
-csvregchem = CSV.parse(csv, :headers => true)
-csv= File.read(Rails.root.join('lib', 'seeds', 'sequences.csv')).scrub
-csvseq = CSV.parse(csv, :headers => true)
-csv = File.read(Rails.root.join('lib', 'seeds', 'primers1.csv')).scrub
-csvprim = CSV.parse(csv, :headers => true)
-csv = File.read(Rails.root.join('lib', 'seeds', 'bacteria2.csv')).scrub
-csvbac= CSV.parse(csv, :headers => true)
-csv = File.read(Rails.root.join('lib', 'seeds', 'mol_biol.csv')).scrub
-csvmolbiol= CSV.parse(csv, :headers => true)
+# require 'csv'
+# csv = File.read(Rails.root.join('lib', 'seeds', 'reg_chem.csv')).scrub
+# csvregchem = CSV.parse(csv, :headers => true)
+# csv= File.read(Rails.root.join('lib', 'seeds', 'sequences.csv')).scrub
+# csvseq = CSV.parse(csv, :headers => true)
+# csv = File.read(Rails.root.join('lib', 'seeds', 'primers1.csv')).scrub
+# csvprim = CSV.parse(csv, :headers => true)
+# csv = File.read(Rails.root.join('lib', 'seeds', 'bacteria2.csv')).scrub
+# csvbac= CSV.parse(csv, :headers => true)
+# csv = File.read(Rails.root.join('lib', 'seeds', 'mol_biol.csv')).scrub
+# csvmolbiol= CSV.parse(csv, :headers => true)
 
-csvregchem.each do |r|
-	t = RegularChemical.new
-	t.short_name = r["name"]
-	t.full_name = r["fullname"]
-	t.formula = r["formula"]
-	t.producer = r["producer"]
-	t.catalogue_number = r["catalogue"]
-	t.place = r["place"]
-	t.comments = r["comments"]
-	t.save
-	puts "Regular chemicals #{t.short_name} added"
-end
+# csvregchem.each do |r|
+# 	t = RegularChemical.new
+# 	t.short_name = r["name"]
+# 	t.full_name = r["fullname"]
+# 	t.formula = r["formula"]
+# 	t.producer = r["producer"]
+# 	t.catalogue_number = r["catalogue"]
+# 	t.place = r["place"]
+# 	t.comments = r["comments"]
+# 	t.save
+# 	puts "Regular chemicals #{t.short_name} added"
+# end
 
-csvmolbiol.each do |m|
-	t = MolBiolChemical.new
-	t.name = m["Fast Digest"]
-	t.producer = m["Producer"]
-	t.catalogue_number = m["Nr kat."]
-	t.comments = m["Uwagi"]
-	t.quantity = m["Zapas "]
-	t.delivery_date = m["delivery"]
-	t.rodzaj = m["type"]
-	t.save
-	puts "Molecular biology chemical #{t.name} added"
-end
+# csvmolbiol.each do |m|
+# 	t = MolBiolChemical.new
+# 	t.name = m["Fast Digest"]
+# 	t.producer = m["Producer"]
+# 	t.catalogue_number = m["Nr kat."]
+# 	t.comments = m["Uwagi"]
+# 	t.quantity = m["Zapas "]
+# 	t.delivery_date = m["delivery"]
+# 	t.rodzaj = m["type"]
+# 	t.save
+# 	puts "Molecular biology chemical #{t.name} added"
+# end
 
-csvseq.each do |r|
-	t = Sequence.new
-	t.name = r["seq"]
-	t.organism = r["organism"]
-	puts "##################### Sequence #{t.name} added ########################"
-	csvprim.each do |prow|
-		if prow["seq"] == t.name
-			p = t.primers.new
-			p.name = prow["nazwa"]
-			p.p_sequence = prow["sekwencja"]
-			p.comments = prow["uwagi"]
-			p.gene_sequence = prow["seq"]
-			p.refine
-			p.calculate_tm
-			p.calculate_length
-			puts "Primer #{p.name} added"
-		end
-	end
-	csvbac.each do |bac|
-		if bac['sequence'] == t.name
-			b = t.bacterial_stocks.new
-			b.number = bac['number']
-			b.wstawka = bac['insert']
-			b.sequence_name = bac['sequence']
-			b.plasmid = bac['plasmid']
-			b.tag = bac['TAG']
-			b.species = bac['species']
-			b.strain = bac['strain']
-			b.antibiotic_resistance = bac['antibiotics resistance']
-			b.methods_of_cloning = bac['methods of cloning']
-			b.source = bac['source']
-			b.comments = bac['comments']
-			puts "Bacterial stock #{b.wstawka} added"
-		end
-	end
-	t.save
-end
+# csvseq.each do |r|
+# 	t = Sequence.new
+# 	t.name = r["seq"]
+# 	t.organism = r["organism"]
+# 	puts "##################### Sequence #{t.name} added ########################"
+# 	csvprim.each do |prow|
+# 		if prow["seq"] == t.name
+# 			p = t.primers.new
+# 			p.name = prow["nazwa"]
+# 			p.p_sequence = prow["sekwencja"]
+# 			p.comments = prow["uwagi"]
+# 			p.gene_sequence = prow["seq"]
+# 			p.refine
+# 			p.calculate_tm
+# 			p.calculate_length
+# 			puts "Primer #{p.name} added"
+# 		end
+# 	end
+# 	csvbac.each do |bac|
+# 		if bac['sequence'] == t.name
+# 			b = t.bacterial_stocks.new
+# 			b.number = bac['number']
+# 			b.wstawka = bac['insert']
+# 			b.sequence_name = bac['sequence']
+# 			b.plasmid = bac['plasmid']
+# 			b.tag = bac['TAG']
+# 			b.species = bac['species']
+# 			b.strain = bac['strain']
+# 			b.antibiotic_resistance = bac['antibiotics resistance']
+# 			b.methods_of_cloning = bac['methods of cloning']
+# 			b.source = bac['source']
+# 			b.comments = bac['comments']
+# 			puts "Bacterial stock #{b.wstawka} added"
+# 		end
+# 	end
+# 	t.save
+# end
