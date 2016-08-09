31badbc3ba0dafdaad16ec127e914c85a3483b3a
Wojciech <wojciech@localhost.localdomain>
2016-08-09 23:14:31 +0200

    dropbox

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.dropbox_app_key = ENV["4qd5n19w4f2gluq"]
+  config.dropbox_app_secret = ENV["41vy1k06wcs8qie"]
+  config.dropbox_access_token = ENV["sxdon0yyih6m3igo"]
+  config.dropbox_access_token_secret = ENV["9o3zff9w8fkgsuo"]
+  config.dropbox_user_id = ENV["592701869"]
+  config.dropbox_access_type = "dropbox"
+end
